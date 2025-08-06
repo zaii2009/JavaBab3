124a1b9f2a0e344fb3d8699cc64f68e6a8fcf638
zaii2009 <arkanzaidan300@gmail.com>
2025-08-06 10:53:28 +0700

    Add files via upload

---
+++ zaidan/src/zaidan/zaidan.java
@@ -0,0 +1,83 @@
+package zaidan ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+
+public class zaidan {
+    public static void main(String[] args) throws IOException {
+//        System.out.println("HALLO");
+//        System.out.println("halo"+" "+"dunia");
+//        System.out.println(2+" "+5);
+//        System.out.println(2+5);
+//        System.out.println("angka"+" "+5);
+//        System.out.println("2+5"+" = "+2+5);
+//        System.out.println("2+5"+" = "+(2+5));
+
+//        String namaDepan = "";
+//        String namaBelakang = "";
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Nama Depan : ");
+//        //membaca inputan user
+//        namaDepan = input.next();
+//        System.out.println("Nama Belakang : ");
+//        //membaca inputan user
+//        namaBelakang = input.next();
+//        System.out.println("Nama Saya adalah :");
+//        System.out.println(namaDepan + " " + namaBelakang);
+
+//        String namaDepan = "";
+//        String namaBelakang = "";
+//        //buat objek dari inputStream
+//        InputStreamReader ireader =
+//        new InputStreamReader(System.in);
+//        //buat objek bufferreader
+//        BufferedReader breader =
+//        new BufferedReader(ireader);
+//        System.out.println("Nama Depan : ");
+//        //membaca inputan user
+//        namaDepan =
+//        breader.readLine();
+//        System.out.println("Nama Belakang : ");
+//        //membaca inputan user
+//        namaBelakang =
+//        breader.readLine();
+//        System.out.println("Nama Saya adalah :");
+//        System.out.println(namaDepan + " " +
+//        namaBelakang);
+//        
+//        String nama =
+//        JOptionPane.showInputDialog("Masukan Nama:");
+//        System.out.println("Nama : "+nama);
+
+        // String a =
+        // JOptionPane.showInputDialog("Masukan nilai untuk variabel a : ");
+        
+        // String b =
+        // JOptionPane.showInputDialog("Masukan nilai untuk variabel b : ");
+        
+        // System.out.println("Hasil :" +
+        // (Integer.parseInt(a) *
+        // Integer.parseInt(b)));
+
+        // String nama = "Budi";
+        // String jurusan = "Sistem Informasi";
+        // JOptionPane.showMessageDialog(null, "Halo " + nama+ ",jurusan Kamu " + jurusan);
+
+        // JOptionPane.showMessageDialog(null, "Sebuah Informasi.","Informasi",JOptionPane.INFORMATION_MESSAGE);
+
+        // JOptionPane.showMessageDialog(null, "Sebuah Error !","Error",JOptionPane.ERROR_MESSAGE);
+
+        // JOptionPane.showMessageDialog(null, "Sebuah Peringatan.","Peringatan",JOptionPane.WARNING_MESSAGE);
+
+        // JOptionPane.showMessageDialog(null, "Sebuah Pertanyaan.", "Pertanyaan",JOptionPane.QUESTION_MESSAGE);
+
+        // JOptionPane.showMessageDialog(null, "Sebuah Plain Message.","Plain Message",JOptionPane.PLAIN_MESSAGE);
+        
+    }
+    
+    
+}
